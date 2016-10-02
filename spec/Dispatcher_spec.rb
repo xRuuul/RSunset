a6a8d2c6ab2c9a7dc3c1c378923a2370bc275416
Raoul Dittmann <raoul.dittmann@gmail.com>
2016-10-03 00:53:56 +0200

    Adding tests to dispatcher

--- spec/Dispatcher_spec.rb
+++ spec/Dispatcher_spec.rb
@@ -4,12 +4,29 @@ describe RSunset::Dispatcher do
 
   dispatcher = RSunset::Dispatcher.new("123")
 
-  it "generates correct api call: CURRENT WEATHER BY CITY NAME" do
+  it "handles server response correctly" do
+
+    request = RSunset::Current_Weather_Request.create_by_city_name("London","uk")
+
+    #Server Error 401
+    stub_request(:get,"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=123").to_return(status: 401)
+    expect{
+      dispatcher.send_request(request)
+    }.to raise_exception(RuntimeError,"Server returning code 401")
+
+  end
+
+  it "generates api call: CURRENT WEATHER BY CITY NAME and parses respone correctly" do
+
+    #Make request
+
     request = RSunset::Current_Weather_Request.create_by_city_name("London","uk")
 
     stub_request(:get,"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=123")
     dispatcher.send_request(request)
 
+    #Parse response
+    #TODO
   end
 
 end
